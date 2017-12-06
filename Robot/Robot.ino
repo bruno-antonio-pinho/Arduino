f075c1768fa2fe5ce008af1689795469bf14d05b
FabianoKraemer <FabianoKraemer@users.noreply.github.com>
2017-12-06 10:41:42 -0200

    Update Robot.ino
    Corrigido para trabalhar com Raspberry

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -13,34 +13,35 @@ Seguidor seguidor_linha;
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 }
 
 void loop() {
-  uint8_t msg[3];
+  uint8_t msg[2];
 
   if (Serial.available()) {
-    Serial.println("Estou aqui: ");
-    Serial.readBytes(msg, 3);
-    if (msg[0] = 125) {
-      int dir = msg[1] - 48;
-      int motor_speed = msg[2];
-      manual_move.move_M(dir, motor_speed, motor_L, motor_R);
-    }
+    Serial.readBytes(msg, 2);
 
-    if(msg[0] = 150){
-      while(msg[0] != 200){
-        Serial.readBytes(msg, 3);      
+
+    if(msg[0] == 49){
+      while(msg[0] != 48){             
         SENSOR1 = analogRead(8) + 200; //200 é o valor do offset
         SENSOR2 = analogRead(9) + 200;
         SENSOR3 = analogRead(10) + 200;
         seguidor_linha.move_linha(motor_L, motor_R, SENSOR1, SENSOR2, SENSOR3);
-        Serial.println("Está no seguidor de linha");
+        Serial.readBytes(msg, 2);
       }
   }
+
+      if (msg[0] == 48) {
+      int dir = msg[1] - 48;
+      int motor_speed = 148;
+      manual_move.move_M(dir, motor_speed, motor_L, motor_R);
+    }
+    
 }
 
 }
